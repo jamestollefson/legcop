df13f8415bf50aa8de96745ceaba9e971e372a70
James L Tollefson <james.tollefson@yahoo.com>
2020-10-30 13:40:53 -0800

    added url and username to setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
     
 setuptools.setup(
-    name = "legcop-USERNAME",
+    name = "legcop-james.l.tollefson",
     version = "0.0.1",
     author = "James Tollefson",
     author_email = "james.l.tollefson@alaskaupdate.com",
     description = "API utilities to access legislative information from across the 54 U.S. States and Territories",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = "",
+    url = "https://github.com/jamestollefson/legcop",
     package = setuptools.find_packages(),
     classifiers = [
         "Programming Language :: Python :: 3",
