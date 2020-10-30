a39cb1c9de8c7f8c1aa9d3eb948bc26d0d722364
James L Tollefson <james.tollefson@yahoo.com>
2020-10-30 15:09:57 -0800

    added tests

--- legcop/tests/tests.py
+++ legcop/tests/tests.py
@@ -1,6 +1,17 @@
-from unittest import TestCase
+import unittest
 
-import legcop
+#import legiscan
 
-class TestTest(TestCase):
-    print("hello, world!")
+class TestFail(unittest.TestCase):
+    def test_fail(self):
+        self.assertEqual(9, 7, "should be false")
+        
+    def test_pass(self):
+        self.assertEqual(9, 9, "should be true")
+        
+if __name__ == '__main__':
+    unittest.main()
+    
+    
+    
+    
